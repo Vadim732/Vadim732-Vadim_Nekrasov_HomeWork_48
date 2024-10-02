fa491838166e4fd392fb0ab8e4c899caa9a6aeef
Nekrasov Vadim <vadim65337.68@gmail.com>
2024-10-02 20:48:23 +0600

    Implemented handler for post request application/json

--- HttpServer/HttpServer/MyServer.cs
+++ HttpServer/HttpServer/MyServer.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net;
+using System.Text.Json;
 using RazorEngine;
 using RazorEngine.Templating;
 
@@ -40,32 +41,60 @@ namespace HttpServer
         private void Process(HttpListenerContext context)
         {
             string filename = context.Request.Url.AbsolutePath;
-            filename = _siteDirectory + filename;
 
-            if (File.Exists(filename))
+            if (context.Request.HttpMethod == "POST" && filename == "/showText.html")
             {
-                try
+                using (var reader = new StreamReader(context.Request.InputStream, context.Request.ContentEncoding))
                 {
-                    string content = BuildHtml(filename, context.Request.QueryString);
-                    context.Response.ContentType = GetContentType(filename);
-                    context.Response.ContentLength64 = System.Text.Encoding.UTF8.GetBytes(content).Length;
-                    byte[] buffer = System.Text.Encoding.UTF8.GetBytes(content);
+                    string requestBody = reader.ReadToEnd();
+                    var jsonDocument = JsonDocument.Parse(requestBody);
+                    string text = jsonDocument.RootElement.GetProperty("text").GetString() ?? "No text provided";
+
+                    string layoutPath = _siteDirectory + "/layout.html";
+                    var razorService = Engine.Razor;
+                    if (!razorService.IsTemplateCached("layout", null))
+                        razorService.AddTemplate("layout", File.ReadAllText(layoutPath));
+                    string html = razorService.RunCompile("<h1>@Model.Text</h1>", "templateKey", null,
+                        new { Text = text });
+
+                    byte[] buffer = System.Text.Encoding.UTF8.GetBytes(html);
+                    context.Response.ContentType = "text/html";
+                    context.Response.ContentLength64 = buffer.Length;
                     context.Response.OutputStream.Write(buffer, 0, buffer.Length);
                     context.Response.OutputStream.Flush();
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-                    context.Response.OutputStream.Write(new byte[0]);
+                    context.Response.OutputStream.Close();
                 }
             }
             else
             {
-                context.Response.StatusCode = 404;
-                context.Response.OutputStream.Write(new byte[0]);
+                filename = _siteDirectory + filename;
+
+                if (File.Exists(filename))
+                {
+                    try
+                    {
+                        string content = BuildHtml(filename, context.Request.QueryString);
+                        context.Response.ContentType = GetContentType(filename);
+                        context.Response.ContentLength64 = System.Text.Encoding.UTF8.GetBytes(content).Length;
+                        byte[] buffer = System.Text.Encoding.UTF8.GetBytes(content);
+                        context.Response.OutputStream.Write(buffer, 0, buffer.Length);
+                        context.Response.OutputStream.Flush();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        context.Response.OutputStream.Write(new byte[0]);
+                    }
+                }
+                else
+                {
+                    context.Response.StatusCode = 404;
+                    context.Response.OutputStream.Write(new byte[0]);
+                }
+
+                context.Response.OutputStream.Close();
             }
-            context.Response.OutputStream.Close();
         }
 
         private string BuildHtml(string filename, System.Collections.Specialized.NameValueCollection query)
