92f53c4aa1bb2173ccfb3439ab57ed87694f7aac
Nekrasov Vadim <vadim65337.68@gmail.com>
2024-10-03 13:33:27 +0600

    Bug fixes

--- HttpServer/HttpServer/Serializer.cs
+++ HttpServer/HttpServer/Serializer.cs
@@ -8,40 +8,28 @@ public class Serializer
 
     public List<Emploeeys> LoadEmployees()
     {
-        if (!File.Exists(path) || new FileInfo(path).Length == 0)
+        if (!File.Exists(path) || new FileInfo(path).Length == 0 || string.IsNullOrWhiteSpace(File.ReadAllText(path)))
         {
-            var newEmployees = new List<Emploeeys>
-            {
-                new Emploeeys { Id = 1, Name = "John Smith", Age = 50 },
-                new Emploeeys { Id = 2, Name = "Anny Jackson", Age = 94 },
-                new Emploeeys { Id = 3, Name = "Freddy Jackson", Age = 19 },
-                new Emploeeys { Id = 4, Name = "Rick Doe", Age = 34 }
-            };
-
-            var json = JsonSerializer.Serialize(newEmployees, new JsonSerializerOptions { WriteIndented = true });
-            File.WriteAllText(path, json);
-
-            return newEmployees;
+            return newEmployees();
         }
         var fileContent = File.ReadAllText(path);
-        
-        if (string.IsNullOrWhiteSpace(fileContent))
-        {
-            var newEmployees = new List<Emploeeys>
-            {
-                new Emploeeys { Id = 1, Name = "John Smith", Age = 50 },
-                new Emploeeys { Id = 2, Name = "Anny Jackson", Age = 94 },
-                new Emploeeys { Id = 3, Name = "Freddy Jackson", Age = 19 },
-                new Emploeeys { Id = 4, Name = "Rick Doe", Age = 34 }
-            };
-
-            var json = JsonSerializer.Serialize(newEmployees, new JsonSerializerOptions { WriteIndented = true });
-            File.WriteAllText(path, json);
-
-            return newEmployees;
-        }
         var employees = JsonSerializer.Deserialize<List<Emploeeys>>(fileContent);
 
         return employees;
+    }
+
+    private List<Emploeeys> newEmployees()
+    {
+        var newEmployees = new List<Emploeeys>
+        {
+            new Emploeeys { Id = 1, Name = "John Smith", Age = 50 },
+            new Emploeeys { Id = 2, Name = "Anny Jackson", Age = 94 },
+            new Emploeeys { Id = 3, Name = "Freddy Jackson", Age = 19 },
+            new Emploeeys { Id = 4, Name = "Rick Doe", Age = 34 }
+        };
+        var json = JsonSerializer.Serialize(newEmployees, new JsonSerializerOptions { WriteIndented = true });
+        File.WriteAllText(path, json);
+
+        return newEmployees;
     }
 }
